70b3404e0053b163b85eebe716e699f06496ddc0
laurglenn <laurenglenn1988@gmail.com>
2024-11-03 15:15:09 -0800

    Update 1_Home.py

--- 1_Home.py
+++ 1_Home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PIL import Image
 
 st.set_page_config(
     page_title="Lauren's Portfolio"
@@ -6,3 +7,15 @@ st.set_page_config(
 
 st.title("Overview")
 st.sidebar.success("Select a page above.")
+
+#Add in other places to view my work
+Links = {
+    "Tableau Public":" https://public.tableau.com/app/profile/lauren.glenn/vizzes",
+    "LinkedIn": "https://linkedin.com",
+    "PowerBI Gallery": "https://github.com",
+}
+
+st.write('\n')
+cols = st.columns(len(Links))
+for index, (platform, link) in enumerate(Links.items()):
+    cols[index].write(f"[{platform}]({link})")
