ae9785545fad538ecfb0cd10322f7c639fb86398
laurglenn <laurenglenn1988@gmail.com>
2024-11-09 20:25:10 -0800

    Update streamlitdash.py

--- sections/streamlitdash.py
+++ sections/streamlitdash.py
@@ -1,4 +1,14 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Streamlit(Python) Sales Dashboard")
+st.title("Streamlit Sales Dashboard", anchor=False)
+st.write(\n)
+st.write("The dashboard below was created in Streamlit using the Python libaries streamlit, pandas and plotly. The data was created using Chat GPT. Under the dashboard some of the steps are listed")
+
+#CONFIGS - Preselect data that will show up in filters
+
+#PULL DATA
+
+
+
 
