c21c136f50762e21a7e2c16f9d22cc624abdc284
laurglenn <laurenglenn1988@gmail.com>
2024-11-09 17:34:39 -0800

    Update 2_Projects.py
    Sidebar Projects

--- pages/2_Projects.py
+++ pages/2_Projects.py
@@ -2,4 +2,7 @@ import streamlit as st
 
 st.title("Projects")
 with st.sidebar:
-  st.write("Tableau Dashboard")
+  st.write("Streamlit(Python) Sales Dashboard")
+  st.write("Tableau Sales Dashboard")
+  st.write("Power BI Sales Dashboard")
+  st.write("Excel Sales Dashboard")
