81a722df965969d64b765c1d47071318f6d8695a
laurglenn <laurenglenn1988@gmail.com>
2024-11-09 20:28:29 -0800

    Update powerbidash.py

--- sections/powerbidash.py
+++ sections/powerbidash.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Power BI Sales Dashboard")
+st.title("Power BI Sales Dashboard", anchor=False)
+st.write('\n')
+st.write("The dashboard below was created in Power BI Desktop. The data was created using Chat GPT. Under the dashboard some of the steps are listed. You can access the actual dashboard on Power BI Gallery here: ")
 
