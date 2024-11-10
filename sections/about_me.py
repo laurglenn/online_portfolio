d78a4c47e44c36811649f63d56ba987f2a0062a4
laurglenn <laurenglenn1988@gmail.com>
2024-11-09 19:49:54 -0800

    Update about_me.py

--- sections/about_me.py
+++ sections/about_me.py
@@ -12,18 +12,4 @@ with col2:
     st.write("Sales Analytics Analyst, .........)
 
 
-#Add in other places to view my work
-Links = {
-    "My Tableau Public":" https://public.tableau.com/app/profile/lauren.glenn/vizzes",
-    "My PowerBI Gallery": "https://github.com",
-    "My LinkedIn": "https://linkedin.com",
-}
-
-#Create columns for contact links
-st.write('\n')
-cols = st.columns(len(Links))
-for index, (platform, link) in enumerate(Links.items()):
-    cols[index].write(f"[{platform}]({link})")
-             
-
 
