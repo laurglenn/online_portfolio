5a4f245a0afc4a2bfda4e45cc2d7d1c901e4a0af
laurglenn <laurenglenn1988@gmail.com>
2024-11-09 20:27:39 -0800

    Update tableaudash.py

--- sections/tableaudash.py
+++ sections/tableaudash.py
@@ -1,3 +1,6 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Tableau Sales Dashboard")
+st.title("Tableau Sales Dashboard", anchor=False)
+st.write('\n')
+st.write("The dashboard below was created in Tableau. The data was created using Chat GPT. Under the dashboard some of the steps are listed. You can access the actual dashboard on Tableau Public here: ")
