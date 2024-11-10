c73e73e7f6cf8444d5e69f6e3bd668c726827c24
laurglenn <laurenglenn1988@gmail.com>
2024-11-09 20:29:45 -0800

    Update 1_Home.py

--- 1_Home.py
+++ 1_Home.py
@@ -32,7 +32,7 @@ project_3_page = st.Page(
 pg = st.navigation(
 {
     "Info":[about_page],
-    "Projects": [project_1_page,project_2_page,project_3_page]
+    "Visualization Projects": [project_1_page,project_2_page,project_3_page]
 }
     
 )
