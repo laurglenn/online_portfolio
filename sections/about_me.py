3c40404a093f31dbcd27377c0ba20ada90d36a1a
laurglenn <laurenglenn1988@gmail.com>
2024-11-09 20:00:05 -0800

    Update about_me.py

--- sections/about_me.py
+++ sections/about_me.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
 #----CREATE SECTIONS FOR ABOUT ME PAGE-----
-col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
+col1, col2 = st.columns(2, gap="small", vertical_alignment="left")
 with col1:
     st.image("./assets/LaurenGlenn_Headshot.jpg", width=230)
 with col2:
-    st.title("Lauren Glenn", anchor=False)
+    st.title("Lauren Glenn", anchor=True)
     st.write("I’m a sales analytics analyst who enjoys bringing data to life through visualizations.")
 
     #Add in other places to view my work
@@ -20,6 +20,9 @@ st.write('\n')
 cols = st.columns(len(Links))
 for index, (platform, link) in enumerate(Links.items()):
     cols[index].write(f"[{platform}]({link})")
+
+#-----EXPERIENCES------#
+
              
 
 
