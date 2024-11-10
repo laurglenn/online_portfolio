c189412cc11d6d79b352980e1a63f2b472e8f72b
laurglenn <laurenglenn1988@gmail.com>
2024-11-09 19:55:07 -0800

    Update about_me.py

--- sections/about_me.py
+++ sections/about_me.py
@@ -4,14 +4,12 @@ st.title("About Me")
 
 
 #----CREATE SECTIONS FOR ABOUT ME PAGE-----
-col1, col2, col3 = st.columns(3, gap="small", vertical_alignment="center")
+col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
 with col1:
     st.image("./assets/LaurenGlenn_Headshot.jpg", width=230)
 with col2:
     st.title("Lauren Glenn", anchor=False)
     st.write("Sales Analytics Analyst, .........")
-with col3:
-    st.title("Links Test")
 
     #Add in other places to view my work
 Links = {
