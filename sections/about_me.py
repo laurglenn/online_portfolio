69a681cf10b90d2657a0c103a2efe6c7e9ef289d
laurglenn <laurenglenn1988@gmail.com>
2024-11-09 19:47:25 -0800

    Update about_me.py

--- sections/about_me.py
+++ sections/about_me.py
@@ -2,6 +2,16 @@ import streamlit as st
 
 st.title("About Me")
 
+
+#----CREATE SECTIONS FOR ABOUT ME PAGE-----
+col1, col2, col3 = st.columns(3, gap="small", vertical_alignment="center")
+with col1:
+    st.image("./assets/img.jpg, width=230)
+with col2:
+    st.title("Lauren Glenn", anchor=False)
+    st.write("Sales Analytics Analyst, .........)
+
+
 #Add in other places to view my work
 Links = {
     "My Tableau Public":" https://public.tableau.com/app/profile/lauren.glenn/vizzes",
@@ -14,8 +24,6 @@ st.write('\n')
 cols = st.columns(len(Links))
 for index, (platform, link) in enumerate(Links.items()):
     cols[index].write(f"[{platform}]({link})")
-
-#----CREATE SECTIONS FOR ABOUT ME PAGE-----
-col1, col2, col3 = st.columns(3, gap="small", vertical_alignment="center")
+             
 
 
