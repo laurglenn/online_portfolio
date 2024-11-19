812e722bf98e6aa28dffdaf287a20a5f349aa050
laurglenn <laurenglenn1988@gmail.com>
2024-11-19 12:17:58 -0800

    Update streamlitdash.py

--- sections/streamlitdash.py
+++ sections/streamlitdash.py
@@ -7,6 +7,11 @@ st.title("Streamlit Sales Dashboard", anchor=False)
 st.write('\n')
 st.write("The dashboard below was created in Streamlit using the Python libaries streamlit, pandas and plotly. The data was created using Chat GPT. Under the dashboard some of the steps are listed")
 
+st.set_page_config(
+    page_title="Makeuprama Sales Dashboard",
+    page_icon="🏂",
+    layout="wide",
+    initial_sidebar_state="expanded")
 #CONFIGS - Preselect data that will show up in filters
 
 #PULL DATA
