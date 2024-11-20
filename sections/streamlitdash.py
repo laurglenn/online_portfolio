1f5403aae3a6c32fc7777b4794222d59e6dbd7e0
laurglenn <laurenglenn1988@gmail.com>
2024-11-19 22:43:31 -0800

    Update streamlitdash.py

--- sections/streamlitdash.py
+++ sections/streamlitdash.py
@@ -62,35 +62,7 @@ top_left_column, top_right_column = st.columns((2, 1))
 with top_left_column:
     column_1, column_2, column_3, column_4 = st.columns(4)
 
-    with column_1:
-        plot_metric(
-            "Total Accounts Receivable",
-            6621280,
-            prefix="$",
-            suffix="",
-            show_graph=True,
-            color_graph="rgba(0, 104, 201, 0.2)",
-        )
-        plot_gauge(1.86, "#0068C9", "%", "Current Ratio", 3)
-
-    with column_2:
-        plot_metric(
-            "Total Accounts Payable",
-            1630270,
-            prefix="$",
-            suffix="",
-            show_graph=True,
-            color_graph="rgba(255, 43, 43, 0.2)",
-        )
-        plot_gauge(10, "#FF8700", " days", "In Stock", 31)
-
-    with column_3:
-        plot_metric("Equity Ratio", 75.38, prefix="", suffix=" %", show_graph=False)
-        plot_gauge(7, "#FF2B2B", " days", "Out Stock", 31)
-        
-    with column_4:
-        plot_metric("Debt Equity", 1.10, prefix="", suffix=" %", show_graph=False)
-        plot_gauge(28, "#29B09D", " days", "Delay", 31)
+   
 
 #CONFIGS - Preselect data that will show up in filters
 
